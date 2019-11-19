8b8036b9ccdb496efbcf5fc7878eee3ad5c491a2
kijima <norimaking777@gmail.com>
2019-11-19 16:06:38 +0900

    修正した。

--- src/roguelike/consts.py
+++ src/roguelike/consts.py
@@ -1,13 +1,13 @@
 import pygame
-import pygame.mixer
 from pygame.locals import *
-
 import os
+
 SCREEN = Rect(0, 0, 640, 480)
 SCR_W = 640
 SCR_H = 320
 TITLE, WINDOWTEXT, FIELD, FULLTEXT, COMMAND = range(5)
-DEFAULT_FONT = "Yu Mincho"
-HOME_DIR = "../../"
-IMG_DIR = (HOME_DIR + "img")
-TEXT_DIR = (HOME_DIR + "data")
+DEFAULT_FONT = "Ricty Diminished Discord"
+run_dir = os.path.split(os.path.abspath(__file__))[0]
+HOME_DIR = os.path.join(run_dir, '../../')
+IMG_DIR = os.path.join(HOME_DIR , "img")
+TEXT_DIR = os.path.join(HOME_DIR , "data")
