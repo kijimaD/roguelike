e32b0877810f861e51782a6233751ac44dab62ed
kijima <norimaking777@gmail.com>
2019-11-20 23:56:24 +0900

    メディアファイルの場所を変更した。 load_soundを追加(途中)。

--- src/roguelike/utils.py
+++ src/roguelike/utils.py
@@ -1,5 +1,6 @@
 import os
 import pygame
+import pygame.mixer
 from roguelike.consts import *
 
 """雑多なツール類"""
@@ -8,9 +9,27 @@ class Utils:
         pass
 
     def load_image(file):
+        """例外処理を組み込んだload"""
         file = os.path.join(IMG_DIR, file)
         try:
             surface = pygame.image.load(file)
         except pygame.error:
             raise SystemExit('Could not load image "%s" %s' %(file, pygame.get_error()))
         return surface
+
+    def load_images(*files):
+        """複数の画像読み込み"""
+        imgs = []
+        for file in files:
+            imgs.append(load_image(file))
+        return imgs
+    
+    def load_sound(file):
+        """例外処理を組み込んだload"""
+        if not pygame.mixer: return dummysound()
+        file = os.path.join(SOUND_DIR, file)
+        try:
+            sound = pygame.mixer.Sound(file)
+        except pygame.error:
+            raise SystemExit('Cound not load sound "%s" %s' %(file, pygame.get_error()))
+        return sound
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -5,6 +5,12 @@ from roguelike.utils import Utils
 class TestUtils(object):
     def test_load_image_raise(self):
         """ファイル入力の例外テスト"""
-        Utils.load_image('ball.jpg') # これは問題ない
+        Utils.load_image('ball.jpg')
         with pytest.raises(SystemExit):
             Utils.load_image('error.jpg')
+
+    def test_load_sound_raise(self):
+        """ファイル入力の例外テスト"""
+        Utils.load_sound('beamgun.mp3')
+        # with pytest.raises(SystemExit):
+        #     Utils.load_image('error.mp3')
